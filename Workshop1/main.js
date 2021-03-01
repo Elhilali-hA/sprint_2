7ef29875a86e4db1880ded2f2109b51ec50eb33b
yassineazedine <yassine.azedine0711@gmail.com>
2021-03-01 16:52:38 +0100

    horloge.html.css.html :Modification

--- Workshop1/main.js
+++ Workshop1/main.js
@@ -1,15 +1,15 @@
 
-let date,hr,min,sec
+let h,m,s;
 
-function youcodeTime(){
-     date=new date()
-     hr =date.getHours() ;
-     min = date.getMinutes() ;
-     sec = date.getSeconds() ;
+function clock(){
+     date=new Date();
+     h =date.getHours() ;
+     m = date.getMinutes() ;
+     s = date.getSeconds() ;
 
-   document.getElementsByClassName("sec").style.transform = "rotate(" +(sec/60)*360 + "deg)";
-   document.getElementsByClassName("min").style.transform = "rotate(" +(min/60)*360 + "deg)";
-   document.getElementsByClassName("hr").style.transform = "rotate(" +(hr/12)*360 + "deg)";
+   document.getElementById("sec").style.transform = "rotate(" +(s/60)*360 + "deg)";
+   document.getElementById("min").style.transform = "rotate(" +(m/60)*360 + "deg)";
+   document.getElementById("hour").style.transform = "rotate(" +(h/12)*360 + "deg)";
 }
-youcodeTime()
-window.setInterval(youcodeTime,1000)
+
+setInterval(clock,1000);

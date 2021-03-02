232ff6c295844acc6f94211b76541b9e9807925b
soufiane fatih <fatihe.soufiane@gmail.com>
2021-03-02 11:05:50 +0100

    filees

--- Workshop2/main2.js
+++ Workshop2/main2.js
@@ -2,9 +2,29 @@ var mouves = document.querySelectorAll(".contenue");
 
 
 
+function texttranslate(){
+  this.classList.toggle("texthabt");
+};
+setTimeout(texttranslate, 7000); 
+
 function zoom(){
    this.classList.toggle("mouvement");
-};
+ };
+   // function myFunction() {
+   //    document.getElementById("myDIV").style.transition = "0rem 2s";
+   //  }
+   // function myFunction() {
+   //    document.getElementById("top").style.transitionProperty = "width, height";
+   //  }
+  //  function transl() {
+  //    if (function zoom(){
+  //     this.classList.toggle("mouvement");
+  //   };)
+  //   this.classList.toggle("translate");
+  //  }
 
 
-mouves.forEach(element =>element.addEventListener('click',zoom));
+mouves.forEach(element => element.addEventListener('click',zoom));
+mouves.forEach(element => element.addEventListener('click',texttranslate));
+
+

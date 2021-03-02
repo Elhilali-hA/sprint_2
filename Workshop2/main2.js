ccf67017df11a6bb15b1722585326bd10fd36383
soufiane fatih <fatihe.soufiane@gmail.com>
2021-03-02 16:44:15 +0100

    modif2

--- Workshop2/main2.js
+++ Workshop2/main2.js
@@ -10,20 +10,6 @@ setTimeout(texttranslate, 7000);
 function zoom(){
    this.classList.toggle("mouvement");
  };
-   // function myFunction() {
-   //    document.getElementById("myDIV").style.transition = "0rem 2s";
-   //  }
-   // function myFunction() {
-   //    document.getElementById("top").style.transitionProperty = "width, height";
-   //  }
-  //  function transl() {
-  //    if (function zoom(){
-  //     this.classList.toggle("mouvement");
-  //   };)
-  //   this.classList.toggle("translate");
-  //  }
-
-
 mouves.forEach(element => element.addEventListener('click',zoom));
 mouves.forEach(element => element.addEventListener('click',texttranslate));
 
